72bab64520bc63191b13a62e1cd5e99e46ce0ac9
Demultiplexer1 <77642176+Demultiplexer1@users.noreply.github.com>
2022-01-07 19:24:59 +0100

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,17 +1,19 @@
-// ignore_for_file: deprecated_member_use
+// ignore_for_file: deprecated_member_use, avoid_print
 import 'package:flutter/material.dart';
-import './question.dart';
+import './quiz.dart';
+import './result.dart';
 
 //void main()
 //{
 //runApp(MyApp());
 //}
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return MyAppState();
   }
 }
@@ -19,46 +21,48 @@ class MyApp extends StatefulWidget {
 class MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   void _answerQuestion() {
+    //var Abool = true;
+    //Abool = false;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < '_questions'.length) {
+      print('We have more questions!');
+    } else {
+      print('no more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favorite color?',
+    const _questions = [
+      {
+        'questionText': 'What\'s your favorite color?',
+        'answers': ['Black', 'Red', 'Green', 'White'],
+      },
       'What\'s your favorite animal?',
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': ['Penguin', 'Rabbit', 'Hamster', 'Horse'],
+      },
+      {
+        'questionText': 'Who\'s your favorite instructor?',
+        'answers': ['Haso', 'Huso', 'Teufik', 'Sadik'],
+      }
     ];
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My first app'),
-        ), //AppBar
-        body: Column(children: [
-          Question(
-            questions[_questionIndex],
-          ),
-          RaisedButton(
-            child: const Text('Answer 1'),
-            onPressed: _answerQuestion,
-          ),
-          RaisedButton(
-            child: const Text('Answer 2'),
-            // ignore: avoid_print
-            onPressed: () => print('Answer 2 chosen!'),
-          ),
-          RaisedButton(
-            child: const Text('Answer 3'),
-            onPressed: () {
-              //...
-              // ignore: avoid_print
-              print('Answer 3 chosen');
-            },
-          ),
-        ]),
-      ),
+          appBar: AppBar(
+            title: const Text('My first app'),
+          ), //AppBar
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions)
+              : const Result()),
     );
   }
 }
